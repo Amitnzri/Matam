adf7f44c8fbdbc0e4486227ad75e458d4dd8164f
Amit <amitnzri@gmail.com>
2019-04-21 18:24:33 +0300

    +eurovision.c

--- Assignment1/src/eurovision.c
+++ Assignment1/src/eurovision.c
@@ -2,3 +2,27 @@
 #include <stdlib.h>
 #include "eurovision.h"
 #include "map.h"
+
+
+/*****************************DataSturctures*********************************/
+sturct eurovision_t{
+
+Map states;
+Map judges;
+List Votes;
+int audiencePercent; //set to one by default
+
+}
+
+
+
+
+
+/*****************************StaticFunctions********************************/
+
+
+
+
+
+
+/*****************************Functions**************************************/

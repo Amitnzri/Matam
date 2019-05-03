62ac89e6b3d3a9bc95b84d1adae66dfee570b5d6
Amit <amitnzri@gmail.com>
2019-05-04 00:32:15 +0300

    judge finished

--- Assignment1/include/judge.h
+++ Assignment1/include/judge.h
@@ -4,32 +4,32 @@
 typedef struct Judge_t *Judge;
 
 
-/*****************************
+/********************************************
 judgeCreate:
-Creates new instance of Judge.
-*****************************/
+Creates new instance of Judge and returns it.
+********************************************/
 Judge judgeCreate(int id,const char* name,int* votes);
 /**********************
 judgeDestroy:
 Free Judge from memory.
 ***********************/
 void judgeDestroy(Judge judge);
-/************************************
+/*****************************************
 judgeGetVotes:
 Returns a copy of judge's votes.
 Caller's responsibility to free after use.
-************************************/
+*****************************************/
 int* judgeGetVotes(Judge judge);
-/************************************
+/*****************************************
 judgeGetName:
 Returns a copy of judge's name.
 Caller's responsibility to free after use.
-************************************/
+*****************************************/
 char* judgeGetName(Judge judge);
-/************************************
+/*******************************
 judgeCopy:
 Creates new copy of given judge.
-************************************/
+*******************************/
 Judge judgeCopy(Judge judge);
 
 #endif
